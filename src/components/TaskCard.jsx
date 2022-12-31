4a95aeb19104e7a61785fec0a1f8e1760395ad78
Kev <kevinserrano010@gmail.com>
2022-12-31 14:08:13 -0300

    Add button for delete task

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,8 +1,16 @@
 function TaskCard({task}) {
+
+    function mostrarAlerta(){
+        alert(task.id)
+    }
+
     return (
         <div>
             <h1>{task.title}</h1>
             <h2>{task.description}</h2>
+            <button onClick={mostrarAlerta}>
+                delete task
+            </button>
         </div>
     )
 }
